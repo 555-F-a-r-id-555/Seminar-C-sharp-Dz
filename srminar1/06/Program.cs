3803dc98c7fc65a5683103c2a1fec0d972c16cd3
Farid Bagirov <makflay85zet@gmail.com>
2023-02-11 01:45:52 +0400

    added 06

---
+++ srminar1/06/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+void maximum(int a, int b, int c)
+{
+    int[] x = { a, b, c };
+    int maximum = a;
+    foreach (int maxi in x) if (maxi > maximum) maximum = maxi;
+    Console.WriteLine($"max = {maximum}");
+}
+
+maximum(2, 3, 7);
+maximum(44, 5, 78);
+maximum(22, 3, 9);
